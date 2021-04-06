20bf29d95b34464200a2d5fdd426f3078a4cc8e2
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-04-05 23:42:50 -0700

    finish part one startGame method and start button event listener

--- js/Game.js
+++ js/Game.js
@@ -21,6 +21,7 @@ If the phrase includes the guessed letter, add the chosen CSS class to the selec
 removeLife(): this method removes a life from the scoreboard, by replacing one of the liveHeart.png images with a lostHeart.png image (found in the images folder) and increments the missed property. If the player has five missed guesses (i.e they're out of lives), then end the game by calling the gameOver() method.
 checkForWin(): this method checks to see if the player has revealed all of the letters in the active phrase.
 gameOver(): this method displays the original start screen overlay, and depending on the outcome of the game, updates the overlay h1 element with a friendly win or loss message, and replaces the overlay’s start CSS class with either the win or lose CSS class. */
+
 class Game {
   constructor() {
     this.missed = 0;
@@ -43,11 +44,21 @@ class Game {
 
     return phrasesArray;
   }
+
   /**
    * Selects random phrase from phrases property
    * @return {Object} Phrase object chosen to be used
    */
   getRandomPhrase() {
     return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+  }
+
+  /**
+   * Begins game by selecting a random phrase and displaying it to user
+   */
+  startGame() {
+    document.getElementById("overlay").style.display = "none";
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
   }
 }
--- js/app.js
+++ js/app.js
@@ -9,14 +9,14 @@
 Create a new instance of the Game class and add event listeners for the start button and onscreen keyboard buttons:
 Add a click event listener to the "Start Game" button which creates a new Game object and starts the game by calling the startGame() method.
 Add click event listeners to each of the onscreen keyboard buttons, so that clicking a button calls the handleInteraction() method on the Game object. Event delegation can also be used in order to avoid having to add an event listener to each individual keyboard button. Clicking the space between and around the onscreen keyboard buttons should not result in the handleInteraction() method being called. */
-
-//Create new instance of the Game class
-//const game = new Game();
+let game;
 
 //Listens for clicks on the start button and starts the game by calling the startGame() method.
-
-//document.getElementById("btn-reset").addEventListener("click", function () {
-// game.startGame();
-//});
+document.getElementById("btn__reset").addEventListener("click", (e) => {
+  //Create new instance of the Game class
+  game = new Game();
+  game.startGame();
+  console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+});
 
 //Listens for clicks onscreen keyboard buttons
