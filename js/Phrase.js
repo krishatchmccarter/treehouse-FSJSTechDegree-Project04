66ac08ad65ce27cc0484adf78bd4b42dca1c82c5
Kris Hatch-McCarter <userkris169@yahoo.com>
2021-04-07 11:29:58 -0700

    checkLetterandshowMatchedLettermethodsinPhrase

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,15 +2,11 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
-//Phrase.js to create a Phrase class to handle the creation of phrases.
+//addPhraseToDisplay(): When the player correctly guesses a letter, the empty box is replaced with the matched letter (see the showMatchedLetter() method below). Make sure the phrase displayed on the screen uses the letter CSS class for letters and the space CSS class for spaces.
 
-/**Create the Phrase class in the Phrase.js file.
-The class should include a constructor that receives a phrase parameter and initializes the following properties:
-phrase: this is the actual phrase the Phrase object is representing. This property should be set to the phrase parameter, but converted to all lower case.
-The class should also have these methods:
-addPhraseToDisplay(): this adds letter placeholders to the display when the game starts. Each letter is presented by an empty box, one li element for each letter. See the example_phrase_html.txt file for an example of what the rendered HTML for a phrase should look like when the game starts, including any id or class attributes needed. When the player correctly guesses a letter, the empty box is replaced with the matched letter (see the showMatchedLetter() method below). Make sure the phrase displayed on the screen uses the letter CSS class for letters and the space CSS class for spaces.
-checkLetter(): checks to see if the letter selected by the player matches a letter in the phrase.
-showMatchedLetter(): reveals the letter(s) on the board that matches the player's selection. To reveal the matching letter(s), select all of the letter DOM elements that have a CSS class name that matches the selected letter and replace each selected element's hide CSS class with the show CSS class. */
+//showMatchedLetter(): reveals the letter(s) on the board that matches the player's selection. To reveal the matching letter(s), select all of the letter DOM elements that have a CSS class name that matches the selected letter and replace each selected element's hide CSS class with the show CSS class. */
+
+//Phrase constructor receives a phrase parameter and initializes phrase property.  phrase is the actual phrase the Phrase object is representing converted to all lower case.
 
 class Phrase {
   constructor(phrase) {
@@ -20,6 +16,9 @@ class Phrase {
   /**
    * Display phrase on game board
    */
+
+  //  loops over the this.phrase property and in the loop use a condition to check if its a space or a letter. sets the text property for those letter li elements and className.
+
   addPhraseToDisplay() {
     const phraseDiv = document.getElementById("phrase");
 
@@ -39,7 +38,32 @@ class Phrase {
     }
   }
 
-  //loop over the this.phrase property and in the loop use a condition to check if its a space or a letter.
+  //checkLetter(): checks to see if the letter selected by the player matches a letter in the phrase.
+  /**
+   * Checks if passed letter is in phrase
+   * @param (string) letter - Letter to check
+   */
+  checkLetter(letter) {
+    if (this.phrase.includes(letter)) {
+      return true;
+    }
+  }
 
-  //set the text property for those letter li elements, when you are setting the className if it's a letter set the text Content of the li element equal to the letter in the phrase.
+  //showMatchedLetter(): Reveals the letter(s) on the board that matches a letter in the phrase.
+
+  /**
+   * Displays passed letter on screen after a match is found
+   * @param (string) letter - Letter to display
+   */
+  showMatchedLetter(letter) {
+    const hiddenLetters = document.getElementsByClassName("hide"); //returns a collection of HTML Elements
+
+    //loop through collection and if className matches letter, toggle show/hide classes
+    for (let i = 0; i < hiddenLetters.length; i++) {
+      if (hiddenLetters[i].className === letter) {
+        hiddenLetters[i].classList.add("show");
+        hiddenLetters[i].classList.remove("hide");
+      }
+    }
+  }
 }
